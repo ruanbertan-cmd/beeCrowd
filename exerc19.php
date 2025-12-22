dbabfd29428a2539e880896f8e051696e2070c8d
Ruan bertan <ruan.bertan@gmail.com>
2025-12-22 00:30:10 -0300

    exercicio 19 concluido

--- exerc19.php
+++ exerc19.php
@@ -1,15 +1,14 @@
 <?php
-
 $notas = [100, 50, 20, 10, 5, 2, 1];
 $valor = (int) fgets(STDIN);
-
+echo $valor . "\n";
 if (0 < $valor && $valor < 1000000) {
-    echo "Ok, valor dentro do esperado!\n";
-
-    foreach ($notas as $nota) {
-        echo $valor / $nota . " nota(s) de R$ " . $nota . "\n";
+    for ($i=0; $i < count($notas); $i++) { 
+        $nota = (int) ($valor / $notas[$i]);
+        $valor = $valor % $notas[$i];
+        echo $nota . " nota(s) de R$ " . number_format($notas[$i], 2,',','') . "\n";
     }
-
 } else {
     echo "Valor inválido!\n";
 }
+?>
