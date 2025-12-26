1c91e1990a3d5bf5cb2ff0964526aeff71c94aa5
Ruan Bertan <ruan.bertan@gmail.com>
2025-12-26 09:28:07 -0300

    exercicio 22 concluido

--- exerc22.php
+++ exerc22.php
@@ -1,14 +1,23 @@
 <?php
-
 $valorMonetario = (float) fgets(STDIN);
-$notas = [100, 50, 20, 10, 5, 2];
-$moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01];
+$valorMonetario = round($valorMonetario * 100);
+
+$notas = [10000, 5000, 2000, 1000, 500, 200];
+$moedas = [100, 50, 25, 10, 5, 1];
 
 echo "NOTAS:". PHP_EOL;
 
 foreach ($notas as $nota) {
-   
     $qtdNotas = intdiv($valorMonetario, $nota);
-    $valorMonetario = $valorMonetario % $nota;
-    echo $qtdNotas . " nota(s) de R$ " . number_format($nota, 2, ".", " ") . PHP_EOL;
+    $valorMonetario %= $nota;
+    echo $qtdNotas . " nota(s) de R$ " . number_format($nota / 100, 2, ".", " ") . PHP_EOL;
 }
+
+echo "MOEDAS:". PHP_EOL;
+
+foreach ($moedas as $moeda) {
+    $qtdMoedas = intdiv($valorMonetario, $moeda);
+    $valorMonetario %= $moeda;
+    echo $qtdMoedas . " moeda(s) de R$ " . number_format($moeda / 100, 2, ".", " ") . PHP_EOL;
+}
+?>
